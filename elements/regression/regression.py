3bb8d2cfdf949ae0f8a139113181ea22bfeebc3f
clot <william.clot@eurecom.fr>
2018-10-22 11:58:43 +0200

    Updates on the regression.py file

--- elements/regression/regression.py
+++ elements/regression/regression.py
@@ -1,16 +1,50 @@
 #!/usr/bin/env python3
 
+""""Ridge Regression test using Python (pandas & numpy)"""
+
+__author__ = "William CLOT, williamclot.github.com"
+__license__ = "MIT"
+__date__ = "22/10/2018"
+
 import pandas as pd
 import numpy as np
 import math
 
-data = pd.read_csv("./../../datasets/forestfires.csv")
-# data = data.sort_values(by=['area'])
+##---------* Functions *----------##
 
-y = data[['area']].values
+def opendata(url):
+    return pd.read_csv(url)
+
+def getA(X, lamb=0.1):
+    '''get A from X and lambda'''
+    d = len(X[0])
+    I = np.eye(d)
+    Xt = np.transpose(X)
+
+    A = np.dot(Xt, X)+lamb*I
+
+    return A, Xt
+
+def getb(Xt, Y):
+    '''get b from Xt and Y'''
+    b = np.dot(Xt, Y)
+
+    return b
+
+
+##--------* Regression *---------##
+
+data = opendata("./../../datasets/forestfires.csv")
+
+Y = data[['area']].values
 
 # Applying the logarithm model to the area
-for i in range(len(y)):
-    y[i] = math.log(y[i]+1)
+for i in range(len(Y)):
+    Y[i] = math.log(Y[i]+1)
 
-print(y)
+X = data[['FFMC', 'DMC', 'DC', 'ISI', 'temp', 'RH', 'wind', 'rain']].values
+
+A, Xt = getA(X)
+b = getb(Xt, Y)
+
+print(A)
