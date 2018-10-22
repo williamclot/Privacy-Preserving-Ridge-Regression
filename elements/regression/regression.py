0176672afefd33be4e45dfeb53ea54b6bcfe2be5
camille plays <plays.camille@gmail.com>
2018-10-22 23:16:37 +0100

    back_substitution

--- elements/regression/regression.py
+++ elements/regression/regression.py
@@ -43,7 +43,7 @@ def cholesky0(A):
     # Perform the Cholesky decomposition
     for row in range(n):
         for col in range(row+1):
-            tmp_sum = np.dot(L[row,:col], L[col,:col])         
+            tmp_sum = np.dot(L[row,:col], L[col,:col])
             if (row == col): # Diagonal elements
                 L[row, col] = math.sqrt(max(A[row,row] - tmp_sum, 0))
             else:
@@ -67,3 +67,44 @@ b = getb(Xt, Y)
 
 L = cholesky0(A)
 print(L)
+
+
+LT = np.transpose(L)
+print(LT)
+
+def back_substitution(LT,b):
+    d = len(X[0])
+    Y = [0]*d
+
+    #last equation of the matrix product
+    Y[d-1]=b[d-1]/LT[d-1][d-1]
+
+    for i in range(d-2,-1,-1):
+        for j in range(d-1,i,-1):
+            b[i]-=LT[i][j]*Y[j]
+        Y[i]=b[i]/LT[i][i]
+
+    return Y
+
+#details of operations in back_substitution
+# yn=b[7]/LT[7][7]
+# print(yn)
+# yn1=(b[6]-LT[6][7]*yn)/LT[6][6]
+# print(yn1)
+# yn2=(b[5]-LT[5][7]*yn-LT[5][6]*yn1)/LT[5][5]
+# print(yn2)
+# yn3=(b[4]-LT[4][7]*yn-LT[4][6]*yn1-LT[4][5]*yn2)/LT[4][4]
+# print(yn3)
+# yn4=(b[3]-LT[3][7]*yn-LT[3][6]*yn1-LT[3][5]*yn2-LT[3][4]*yn3)/LT[3][3]
+# print(yn4)
+# yn5=(b[2]-LT[2][7]*yn-LT[2][6]*yn1-LT[2][5]*yn2-LT[2][4]*yn3-LT[2][3]*yn4)/LT[2][2]
+# print(yn5)
+# yn6=(b[1]-LT[1][7]*yn-LT[1][6]*yn1-LT[1][5]*yn2-LT[1][4]*yn3-LT[1][3]*yn4-LT[1][2]*yn5)/LT[1][1]
+# print(yn6)
+# yn7=(b[0]-LT[0][7]*yn-LT[0][6]*yn1-LT[0][5]*yn2-LT[0][4]*yn3-LT[0][3]*yn4-LT[0][2]*yn5-LT[0][1]*yn6)/LT[0][0]
+# print(yn7)
+
+Y = back_substitution(LT,b)
+print("y=",Y)
+beta = back_substitution(L,Y)
+print("beta=",beta)
