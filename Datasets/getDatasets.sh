fed615dba32222361bd3b09698c3ea8e0ed69274
William CLOT <contact@williamclot.com>
2018-11-07 14:43:55 +0100

    adding a few lines + new dataset implementation

--- Datasets/getDatasets.sh
+++ Datasets/getDatasets.sh
@@ -2,3 +2,6 @@
 
 # ForestFires Dataset
 wget https://archive.ics.uci.edu/ml/machine-learning-databases/forest-fires/forestfires.csv
+
+# Concrete Strenght Dataset
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/concrete/compressive/Concrete_Data.xls
--- Modules/regression/concrete.py
+++ Modules/regression/concrete.py
@@ -30,8 +30,9 @@ def prepareValues(train_frac=0.8, verbose=False):
     '''
     # Opening up the dataset using pandas to easily manipulate Dataframe variables
     print(termcol.HEADER + "Opening up the dataset..."+termcol.ENDC)
-    dataset = pd.read_csv("../../datasets/forestfires.csv")
-    if(verbose): print(dataset.head(5), '...')
+    file = r'../../Datasets/Concrete_Data.xls'
+    dataset = pd.read_excel(file)
+    if(verbose): print(dataset)
     
     # Randomizing the rows of the dataset (separation between training a testing dataset)
     print(termcol.HEADER + "Shuffling the index of the dataset..."+termcol.ENDC)
@@ -39,13 +40,12 @@ def prepareValues(train_frac=0.8, verbose=False):
     if(verbose): print(dataset.head(5), '...')
 
     # Extracting useful data (X, Y)
-    Y = dataset[['area']]
-    # Applying the log model to the area using pandas apply() function
-    Y = Y.apply(lambda y: np.log(y + 1))
-    X = dataset[['FFMC', 'DMC', 'DC', 'ISI', 'temp', 'RH', 'wind', 'rain']]
+    data_lenght = dataset.shape[0]
+    Y = dataset.iloc[:,-1]
+    X = dataset.iloc[0:data_lenght-1]
 
     # Separation between train dataset and test dataset with train_frac
-    data_lenght = dataset.shape[0]
+    
     index_separation = int(data_lenght * train_frac)
     Xtrain = X.iloc[:index_separation]
     Ytrain = Y.iloc[:index_separation]
@@ -56,12 +56,12 @@ def prepareValues(train_frac=0.8, verbose=False):
 
 
 X, Y, Xtest, Ytest = prepareValues(verbose=True)
-Regression = rd.Regression(X,Y,verbose=True)
-Regression.train_model()
+# Regression = rd.Regression(X,Y,verbose=True)
+# Regression.train_model()
 
-print(termcol.WARNING+ "beta :"+ termcol.ENDC)
-print(Regression.beta)
+# print(termcol.WARNING+ "beta :"+ termcol.ENDC)
+# print(Regression.beta)
 
 
-result = Regression.test_model(Xtest,Ytest, func = lambda x: np.exp(x)-1)
-print(result)
+# result = Regression.test_model(Xtest,Ytest, func = lambda x: np.exp(x)-1)
+# print(result)
