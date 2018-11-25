7140a553ee89a128ee60978bea19e4cee86fd736
William CLOT <contact@williamclot.com>
2018-11-25 17:18:11 +0100

    Trying a matrix substraction code with ABY

---
+++ Modules/ABY/examples/matrix_sub/common/matrix_sub.cpp
@@ -0,0 +1,128 @@
+#include "matrix_sub.h"
+#include <abycore/sharing/sharing.h>
+
+int32_t test_matrix_sub_circuit(e_role role, const std::string& address, uint16_t port, seclvl seclvl,
+		uint32_t nvals, uint32_t bitlen, uint32_t nthreads, e_mt_gen_alg mt_alg,
+		e_sharing sharing, uint32_t num) {
+
+	/**
+	 Step 1: Create the ABYParty object which defines the basis of all the
+	 operations which are happening.	Operations performed are on the
+	 basis of the role played by this object.
+	 */
+	ABYParty* party = new ABYParty(role, address, port, seclvl, bitlen, nthreads,
+			mt_alg);
+
+	/**
+	 Step 2: Get to know all the sharing types available in the program.
+	 */
+	std::vector<Sharing*>& sharings = party->GetSharings();
+
+	/**
+	 Step 3: Create the circuit object on the basis of the sharing type
+	 being inputed.
+	 */
+	ArithmeticCircuit* circ =
+			(ArithmeticCircuit*) sharings[sharing]->GetCircuitBuildRoutine();
+
+	/**
+	 Step 4: Creating the share objects - s_x_vec, s_y_vec which
+	 are used as inputs to the computation. Also, s_out which stores the output.
+	 */
+
+	share *s_x_vec, *s_y_vec, *s_out;
+
+	/**
+	 Step 5: Allocate the xvals and yvals that will hold the plaintext values.
+	 */
+	uint16_t x, y;
+
+	uint16_t output, v_sum = 0;
+
+	std::vector<uint16_t> xvals(num);
+	std::vector<uint16_t> yvals(num);
+
+	uint32_t i;
+	srand(time(NULL));
+
+	/**
+	 Step 6: Fill the arrays xvals and yvals with the generated random values.
+	 Both parties use the same seed, to be able to verify the
+	 result. In a real example each party would only supply
+	 one input value. Copy the randomly generated vector values into the respective
+	 share objects using the circuit object method PutINGate().
+	 Also mention who is sharing the object.
+	 The values for the party different from role is ignored,
+	 but PutINGate() must always be called for both roles.
+	 */
+	for (i = 0; i < num; i++) {
+
+		x = rand();
+		y = rand();
+
+		v_sum += x * y;
+
+		xvals[i] = x;
+		yvals[i] = y;
+	}
+
+	s_x_vec = circ->PutSIMDINGate(num, xvals.data(), 16, SERVER);
+	s_y_vec = circ->PutSIMDINGate(num, yvals.data(), 16, CLIENT);
+
+	/**
+	 Step 7: Call the build method for building the circuit for the
+	 problem by passing the shared objects and circuit object.
+	 Don't forget to type cast the circuit object to type of share
+	 */
+	s_out = BuildMatrixSubCircuit(s_x_vec, s_y_vec, num,
+			(ArithmeticCircuit*) circ);
+
+	/**
+	 Step 8: Output the value of s_out (the computation result) to both parties
+	 */
+	s_out = circ->PutOUTGate(s_out, ALL);
+
+	/**
+	 Step 9: Executing the circuit using the ABYParty object evaluate the
+	 problem.
+	 */
+	party->ExecCircuit();
+
+	/**
+	 Step 10: Type caste the plaintext output to 16 bit unsigned integer.
+	 */
+	output = s_out->get_clear_value<uint16_t>();
+
+	std::cout << "\nCircuit Result: " << output;
+	std::cout << "\nVerification Result: " << v_sum << std::endl;
+
+	delete s_x_vec;
+	delete s_y_vec;
+	delete party;
+
+	return 0;
+}
+
+/*
+ Constructs the inner product circuit. num multiplications and num additions.
+ */
+share* BuildMatrixSubCircuit(share *s_x, share *s_y, uint32_t num, ArithmeticCircuit *ac) {
+	uint32_t i;
+
+	// pairwise multiplication of all input values
+	s_x = ac->PutMULGate(s_x, s_y);
+
+	// split SIMD gate to separate wires (size many)
+	s_x = ac->PutSplitterGate(s_x);
+
+	// add up the individual multiplication results and store result on wire 0
+	// in arithmetic sharing ADD is for free, and does not add circuit depth, thus simple sequential adding
+	for (i = 1; i < num; i++) {
+		s_x->set_wire_id(0, ac->PutADDGate(s_x->get_wire_id(0), s_x->get_wire_id(i)));
+	}
+
+	// discard all wires, except the addition result
+	s_x->set_bitlength(1);
+
+	return s_x;
+}
---
+++ Modules/ABY/examples/matrix_sub/common/matrix_sub.h
@@ -0,0 +1,41 @@
+#ifndef __MATRIXSUB_H_
+#define __MATRIXSUB_H_
+
+#include <abycore/circuit/booleancircuits.h>
+#include <abycore/circuit/arithmeticcircuits.h>
+#include <abycore/circuit/circuit.h>
+#include <abycore/aby/abyparty.h>
+#include <math.h>
+#include <cassert>
+
+
+/**
+ \param		role 		role played by the program which can be server or client part.
+ \param 	address 	IP Address
+ \param 	seclvl 		Security level
+ \param 	nvals		Number of values
+ \param 	bitlen		Bit length of the inputs
+ \param 	nthreads	Number of threads
+ \param		mt_alg		The algorithm for generation of multiplication triples
+ \param 	sharing		Sharing type object
+ \param 	num			the number of elements in the inner product
+ \brief		This function is used for running a testing environment for solving the
+ Inner Product.
+ */
+int32_t test_matrix_sub_circuit(e_role role, const std::string& address, uint16_t port, seclvl seclvl,
+		uint32_t nvals, uint32_t bitlen, uint32_t nthreads, e_mt_gen_alg mt_alg,
+		e_sharing sharing, uint32_t num);
+
+/**
+ \param		s_x			share of X values
+ \param		s_y 		share of Y values
+ \param 	num			the number of elements in the inner product
+ \param		ac	 		Arithmetic Circuit object.
+ \brief		This function is used to build and solve the Inner Product modulo 2^16. It computes the inner product by
+ 	 	 	multiplying each value in x and y, and adding those multiplied results to evaluate the inner
+ 	 	 	product. The addition is performed in a tree, thus with logarithmic depth.
+ */
+share* BuildMatrixSubCircuit(share *s_x, share *s_y, uint32_t num, ArithmeticCircuit *ac);
+
+
+#endif
