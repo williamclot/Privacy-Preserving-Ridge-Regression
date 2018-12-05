9ceafa6bd475527f82b8c224b0989112e883ab52
William CLOT <contact@williamclot.com>
2018-12-05 17:25:56 +0100

    adding comments on main program

--- Modules/ABY/examples/sqrt_william/common/circuit.h
+++ Modules/ABY/examples/sqrt_william/common/circuit.h
@@ -15,8 +15,7 @@
 void test_circuit(e_role role, const std::string& address, uint16_t port, seclvl seclvl, uint32_t nvals, uint32_t nthreads,
 	e_mt_gen_alg mt_alg, e_sharing sharing, std::vector<double> data);
 
-share* MatrixSubstraction(share *s_A, share *s_B, BooleanCircuit *bc, uint32_t nvals);
+share* MatrixSubstraction(share *s_A, BooleanCircuit *bc, uint32_t nvals);
 
-share* SqurtApprox(share *s_x, share *half, uint32_t step, uint32_t bitlen, ArithmeticCircuit *ac, BooleanCircuit *bc, Circuit *yc); 
-
-share* ExtractIndex(share *s_x , uint32_t i, uint32_t bitlen, ArithmeticCircuit *ac);
+share* extract_index(share *s_x , uint32_t i, uint32_t bitlen, ArithmeticCircuit *ac);
+share* sqrt_approx(share *s_x, share *half, uint32_t step, uint32_t bitlen, ArithmeticCircuit *ac, BooleanCircuit *bc, Circuit *yc); 
